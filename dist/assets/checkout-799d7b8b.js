1e6c6a6db9a3babbed022c6cc3205719e737170f
Bashir-Danish <bashirdanish124@gmail.com>
2023-12-27 20:33:33 +0430

    issue fixed

---
+++ dist/assets/checkout-799d7b8b.js
@@ -0,0 +1,1 @@
+import{_ as c,o as e,c as o}from"./index-1b99f42b.js";const t={};function n(r,s){return e(),o("div",null," checkout ")}const a=c(t,[["render",n]]);export{a as default};
--- dist/assets/checkout-dc1aa00a.js
+++
@@ -1,1 +0,0 @@
-import{_ as c,o as e,c as o}from"./index-bb87d01e.js";const t={};function n(r,s){return e(),o("div",null," checkout ")}const a=c(t,[["render",n]]);export{a as default};
